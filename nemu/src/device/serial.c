f76ff62ea87325054ba19ebe7a420436767a98cd
tracer-ysyx <tracer@ysyx.org>
2025-10-05 23:47:29 +0800

    > compile NEMU ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:47:29 up  6:03,  1 user,  load average: 0.01, 0.05, 0.08

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -25,7 +25,8 @@ static uint8_t *serial_base = NULL;
 
 
 static void serial_putc(char ch) {
-  MUXDEF(CONFIG_TARGET_AM, putch(ch), putc(ch, stderr));
+  //MUXDEF(CONFIG_TARGET_AM, putch(ch), putc(ch, stderr));
+  MUXDEF(CONFIG_TARGET_AM, putch(ch), ;);
 }
 
 //modified @ 2025.10.05 by jason Y.
