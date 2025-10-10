26797a5c48a7a16c0ff3c8f77aeaac4ab8bb4916
tracer-ysyx <tracer@ysyx.org>
2025-10-10 16:13:22 +0800

    > task:convert in npc ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:13:22 up  2:59,  1 user,  load average: 0.45, 0.22, 0.13

--- npc/wavesim_file/engine/engine.h
+++ npc/wavesim_file/engine/engine.h
@@ -11,7 +11,7 @@
 #include <psram/psram.h>
 #include <sdram/sdram.h>
 #include <register/register.h>
-//#include <sdb/sdb.h>
+#include <sdb/sdb.h>
 //sdb.h和engine.h陷入循环调用
 //sdb.c需要调用engine.c中定义的exec_once，而engine.c需要调用watchpoint.c中定义的与监视点相关的函数
 //#include <difftest/difftest.h>
