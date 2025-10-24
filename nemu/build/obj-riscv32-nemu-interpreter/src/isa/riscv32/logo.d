1e0342263d475c7d5753120c5e1fbaf803229aa5
tracer-ysyx <tracer@ysyx.org>
2025-10-24 18:02:56 +0800

    > compile NEMU ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:02:56 up 37 min,  1 user,  load average: 0.16, 0.05, 0.09

---
+++ nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.d
@@ -0,0 +1,9 @@
+cmd_/home/jason/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o := unused
+
+source_/home/jason/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o := src/isa/riscv32/logo.c
+
+deps_/home/jason/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o := \
+
+/home/jason/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o: $(deps_/home/jason/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o)
+
+$(deps_/home/jason/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o):
