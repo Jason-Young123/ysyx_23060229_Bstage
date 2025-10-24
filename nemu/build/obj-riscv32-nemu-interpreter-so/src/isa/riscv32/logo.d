37294cf32ed30e1b8a2131ce3436c8afe76182f7
NJU_Yang Jiaxin <2098543558@qq.com>
2025-10-24 18:58:36 +0800

    delete nemu/build

--- nemu/build/obj-riscv32-nemu-interpreter-so/src/isa/riscv32/logo.d
+++
@@ -1,9 +0,0 @@
-cmd_/home/jason/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter-so/src/isa/riscv32/logo.o := unused
-
-source_/home/jason/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter-so/src/isa/riscv32/logo.o := src/isa/riscv32/logo.c
-
-deps_/home/jason/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter-so/src/isa/riscv32/logo.o := \
-
-/home/jason/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter-so/src/isa/riscv32/logo.o: $(deps_/home/jason/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter-so/src/isa/riscv32/logo.o)
-
-$(deps_/home/jason/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter-so/src/isa/riscv32/logo.o):
