bcb61d6577194ec8284a2c54d7bd21ffe7351ec9
tracer-ysyx <tracer@ysyx.org>
2025-11-02 14:55:39 +0800

    > compile NEMU ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.8.0-86-generic #87~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Sep 29 09:48:07 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:55:39 up 38 min,  1 user,  load average: 0.10, 0.06, 0.14

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -26,7 +26,8 @@ $(BINARY):: compile_git
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
-override ARGS +=  --batch
+#override ARGS +=  --batch
+#默认不采用batch mode
 
 # Command to execute NEMU
 IMG ?=
