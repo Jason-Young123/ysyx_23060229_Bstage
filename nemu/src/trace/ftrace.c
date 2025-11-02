8e61b92666096afb06bc990f7fb1ef76341c8e09
tracer-ysyx <tracer@ysyx.org>
2025-11-02 16:25:23 +0800

    > compile NEMU ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.8.0-86-generic #87~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Sep 29 09:48:07 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:25:23 up  2:08,  1 user,  load average: 0.23, 0.14, 0.10

--- nemu/src/trace/ftrace.c
+++ nemu/src/trace/ftrace.c
@@ -105,7 +105,7 @@ void init_ftrace(const char* file_path){
      * */
     NO_FENTRY = parse_symtab_entries(symtab_hdr.sh_size, symtab, strtab_hdr.sh_size, strtab, funcs);
 	printf("%d\n", NO_FENTRY);
-	printf("%u - %u - %s\n", funcs[0].value, funcs[0].size, funcs[0].name);
+	printf("%x - %u - %s\n", funcs[0].value, funcs[0].size, funcs[0].name);
 
     fclose(file);
 	#pragma GCC diagnostic pop
