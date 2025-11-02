4478a00b9e886f422d00f415972785bd1bcc21f8
tracer-ysyx <tracer@ysyx.org>
2025-11-02 16:29:56 +0800

    > compile NEMU ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.8.0-86-generic #87~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Sep 29 09:48:07 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:29:56 up  2:12,  1 user,  load average: 0.15, 0.10, 0.09

--- nemu/src/trace/ftrace.c
+++ nemu/src/trace/ftrace.c
@@ -118,7 +118,7 @@ void init_ftrace(const char* file_path){
 //检测pc所在区域是否为func
 void check_pc(int32_t pc, bool *success, uint32_t *value, char **func_name){
     for(int i = 0; i < NO_FENTRY; ++i){
-        if(pc >= funcs[i].value && pc < funcs[i].value + funcs[i].size){
+        if((uint32_t)pc >= funcs[i].value && (uint32_t)pc < funcs[i].value + funcs[i].size){
             *success = true; *func_name = funcs[i].name; *value = funcs[i].value;
 			printf("okok\n");
             return;
