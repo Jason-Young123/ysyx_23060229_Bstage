c552083b83bd5c1b4553ce1c2c8cdf3f66e92d5a
tracer-ysyx <tracer@ysyx.org>
2025-11-02 16:14:31 +0800

    > compile NEMU ysyx_23060229 杨佳鑫 Linux jason-virtual-machine 6.8.0-86-generic #87~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Sep 29 09:48:07 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:14:31 up  1:57,  1 user,  load average: 0.44, 0.16, 0.09

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -30,7 +30,7 @@ image: $(IMAGE).elf
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: insert-arg
-	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS) -e $(IMAGE).elf" IMG=$(IMAGE).bin
 
 gdb: insert-arg
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
